10f6e29ed1c1ff26770685f8ee31f05cddb1ed39
Ben <lolitsaburrito@gmail.com>
2022-02-11 12:16:50 -0800

    init rewrite of PS2.{Socket, SocketClient}. New tests passing

--- lib/ps2/socket_client.ex
+++ lib/ps2/socket_client.ex
@@ -1,25 +1,30 @@
 defmodule PS2.SocketClient do
   @moduledoc ~S"""
-  A module that handles all interaction with Daybreak Games' Planetside 2 Event Streaming service.
+  A behaviour for handling events from a `PS2.Socket`.
 
   ## Implementation
-  To handle incoming game events, your module should `use PS2.SocketClient` and call `PS2.SocketClient.start_link/2`, passing
-  the desired subscription info (Example implementation below). Events will now be sent to your SocketClient, which you handle
-  though `handle_event/1`. Note that you should have a catch-all `handle_event/1` callback in the case of unhandled events
-  (see example), otherwise the client will crash whenever it receives an unhandled event.
+  To handle incoming game events, you need to pass a module with a `handle_event/1` callback to `PS2.Socket` when you
+  start it. This behaviour provides an outline for developing such a module (Example implementation below). Events will
+  be passed to your client module via the `handle_event/1` in a tuple with the form of `{event_name, payload}`. Note
+  that you should have a catch-all `handle_event/1` callback in the case of unhandled events (see example), otherwise
+  the client will crash whenever it receives an unknown event.
+
+  Once you have a module like the one below, pass it under the `:clients` option to your `PS2.Socket` (see that module's
+  documentation for details).
 
   Example implementation:
   ```elixir
   defmodule MyApp.EventStream do
-    use PS2.SocketClient
+    @behaviour PS2.SocketClient
 
-  	def start_link do
-  		subscriptions = [
-  			events: ["PlayerLogin"],
-  			worlds: ["Connery", "Miller", "Soltech"],
-  			characters: ["all"]
-  		]
-      PS2.SocketClient.start_link(__MODULE__, subscriptions)
+    # An optional callback for further filtering of events from PS2.Socket
+    @impl PS2.SocketClient
+    def subscriptions do
+      [
+        events: ["all"],
+        worlds: [PS2.connery],
+        characters: ["all"]
+      ]
     end
 
     @impl PS2.SocketClient
@@ -34,15 +39,9 @@ defmodule PS2.SocketClient do
     end
   end
   ```
-  The second param of `PS2.SocketClient.start_link/2` is the subscription info your client is interested in. See the link below
-  to find a list of all event names. You may also specify "all" in any of the subscription fields (Note: if a field is missing,
-  "all" will be the default.) If you want to receive heartbeat messages (which contain world status updates), include "heartbeat"
-  in your event subscriptions.
-
-  For more information, see the official documentation: https://census.daybreakgames.com/#websocket-details
+  For more information about events and their payloads, see the official documentation:
+  https://census.daybreakgames.com/#websocket-details
   """
-
-  @callback handle_event(event) :: any
 
   @typedoc """
   A two-element tuple representing an in-game event.
@@ -71,78 +70,5 @@ defmodule PS2.SocketClient do
   """
   @type subscription_list :: [subscription] | []
 
-  @world_map %{
-    "Connery" => "1",
-    "Miller" => "10",
-    "Cobalt" => "13",
-    "Emerald" => "17",
-    "Jaeger" => "19",
-    "Briggs" => "25",
-    "Soltech" => "40",
-    "all" => "all"
-  }
-
-  @enforce_keys :pid
-  defstruct [:pid, events: ["all"], worlds: ["all"], characters: ["all"]]
-
-  @doc """
-  Starts the client process, subscribing to the event stream and listens for relevant events.
-  """
-  @spec start_link(atom, subscription_list) :: {:ok, pid}
-  def start_link(module, subscriptions) when not is_nil(subscriptions) do
-    pid =
-      spawn(fn ->
-        struct_opts =
-          Keyword.put(subscriptions, :pid, self())
-          |> Keyword.update(:worlds, ["all"], &world_ids_from_name/1)
-
-        if not is_nil(name = Keyword.get(subscriptions, :name)),
-          do: Process.register(self(), name)
-
-        WebSockex.cast(PS2.Socket, {:subscribe, struct(PS2.SocketClient, struct_opts)})
-        proc_loop(module, subscriptions)
-      end)
-
-    {:ok, pid}
-  end
-
-  @doc false
-  defp proc_loop(module, subscriptions) do
-    receive do
-      {:GAME_EVENT, event} ->
-        Task.start(fn -> module.handle_event(event) end)
-        proc_loop(module, subscriptions)
-
-      _ ->
-        proc_loop(module, subscriptions)
-    end
-  end
-
-  defp world_ids_from_name(worlds) do
-    Enum.map(worlds, &Map.get(@world_map, &1)) |> Enum.filter(&(&1 !== nil))
-  end
-
-  def child_spec(opts) do
-    %{
-      id: __MODULE__,
-      start: {__MODULE__, :start_link, [opts]}
-    }
-  end
-
-  defmacro __using__(_args) do
-    quote location: :keep do
-      @behaviour PS2.SocketClient
-
-      def child_spec(opts) do
-        %{
-          id: __MODULE__,
-          start: {__MODULE__, :start_link, [opts]}
-        }
-      end
-
-      def handle_event(_event), do: :ok
-
-      defoverridable handle_event: 1, child_spec: 1
-    end
-  end
+  @callback handle_event(event) :: any
 end
--- mix.exs
+++ mix.exs
@@ -19,7 +19,6 @@ defmodule PS2.MixProject do
   def application do
     [
       extra_applications: [:logger],
-      mod: {PS2, []}
     ]
   end
 
